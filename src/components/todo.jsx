0a2690768e28b2183bb6e40efdd55ba2600d5d84
Collins Muchui <muchuicollins56@gmail.com>
2023-05-26 12:47:38 +0300

    commiting fils from local machine

---
+++ src/components/todo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Todo= (props) =>{
+  const {todo}=probs
+
+  return(
+    <div className='todos'>
+      <h4>todos</h4>
+      <Todo/>
+
+    </div>
+  )
+}
+
+export default Todo
